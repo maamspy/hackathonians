7ea14ed938b72744475687fdd9cad83a61b75f14
maamspy <mdalaminmollah000@gmail.com>
2026-09-16 02:22:25 +0600

    Remove unused

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono, Josefin_Sans } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const josefin = Josefin_Sans({
   variable: "--font-josefin",
@@ -23,10 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${josefin.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${josefin.variable} h-full antialiased`}>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
