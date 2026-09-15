09cf439920c556e33fccaa3b6635dc9c85e2a659
maamspy <mdalaminmollah000@gmail.com>
2026-09-16 02:25:42 +0600

    Add target prop to Button component for customizable link behavior

--- components/button.js
+++ components/button.js
@@ -4,11 +4,16 @@ const COLORS = {
   yellow: "bg-brand-yellow",
 };
 
-export default function Button({ color = "blue", href, children }) {
+export default function Button({
+  color = "blue",
+  href,
+  target = "_blank",
+  children,
+}) {
   return (
     <a
       href={href}
-      target="_blank"
+      target={target}
       rel="noopener noreferrer"
       className={`block w-full cursor-pointer px-5 py-3 text-base font-bold text-brand-white md:w-auto md:px-6 md:text-lg ${
         color === "yellow" ? "text-brand-black" : ""
